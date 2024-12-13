5c0dbb5701371a57215cdd090d3fe694129829e2
kaspazza <kaspazza@gmail.com>
2024-12-13 12:44:08 +0100

    Day 6 part 2

--- src/kaspazza/2024/6/solution.clj
+++ src/kaspazza/2024/6/solution.clj
@@ -37,12 +37,12 @@
 (defn mark-elements
   [m]
   (map-indexed
-    (fn [row-idx row]
-      (map-indexed
-        (fn [col-idx el]
-          {:x col-idx, :y row-idx, :el el, :el-type (map-el-type el)})
-        row))
-    m))
+   (fn [row-idx row]
+     (map-indexed
+      (fn [col-idx el]
+        {:x col-idx, :y row-idx, :el el, :el-type (map-el-type el)})
+      row))
+   m))
 
 (defn guard-up
   [obstacles {guard-x :x, guard-y :y, :as _guard}]
@@ -52,11 +52,11 @@
                                   (sort-by :y)
                                   last)]
     {:visited-positions
-       (into #{} (map (fn [num] {:x x, :y num}) (range (inc y) (inc guard-y)))),
+     (into #{} (map (fn [num] {:x x, :y num}) (range (inc y) (inc guard-y)))),
      :guard-pos {:y (inc y), :x x, :direction :right}}
     {:guard-pos nil,
      :visited-positions
-       (into #{} (map (fn [num] {:x guard-x, :y num}) (range 0 guard-y)))}))
+     (into #{} (map (fn [num] {:x guard-x, :y num}) (range 0 guard-y)))}))
 
 (defn guard-right
   [obstacles max-x {guard-x :x, guard-y :y, :as _guard}]
@@ -66,8 +66,8 @@
                                   (sort-by :x)
                                   first)]
     {:visited-positions
-     (into #{} (map (fn [num] {:x num, :y y}) (range guard-x (inc x)))),
-     :guard-pos {:y y, :x (inc x), :direction :down}}
+     (into #{} (map (fn [num] {:x num, :y y}) (range guard-x (dec x)))),
+     :guard-pos {:y y, :x (dec x), :direction :down}}
     {:guard-pos nil,
      :visited-positions
      (into #{} (map (fn [num] {:x num, :y guard-y}) (range guard-x max-x)))}))
@@ -80,22 +80,19 @@
                                   (sort-by :y)
                                   first)]
     {:visited-positions
-     (into #{} (map (fn [num] {:x x, :y num}) (range guard-y (inc y)))),
-     :guard-pos {:y (inc y), :x x, :direction :right}}
+     (into #{} (map (fn [num] {:x x, :y num}) (range guard-y y))),
+     :guard-pos {:y (dec y), :x x, :direction :left}}
     {:guard-pos nil,
      :visited-positions
      (into #{} (map (fn [num] {:x guard-x, :y num}) (range guard-y (inc max-y))))}))
-
-
-
 
 (defn guard-left
   [obstacles {guard-x :x, guard-y :y, :as _guard}]
   (if-let [{:keys [x y]} (some->> obstacles
                                   (filter (fn [{:keys [x y], :as _obstacle}]
-                                            (and (> x guard-x) (= y guard-y))))
+                                            (and (< x guard-x) (= y guard-y))))
                                   (sort-by :x)
-                                  first)]
+                                  last)]
     {:visited-positions
      (into #{} (map (fn [num] {:x num, :y y}) (range (inc x) (inc guard-x)))),
      :guard-pos {:y y, :x (inc x), :direction :up}}
@@ -104,24 +101,32 @@
      (into #{} (map (fn [num] {:x num, :y guard-y}) (range 0 guard-x)))}))
 
 
-
 (defn visited-positions
   [{:keys [max-x max-y]} obstacles guard]
   (loop [g guard
          visited-pos #{}]
-    (if-let [guard-direction (or (:direction g) (get guard-patterns (:el g)))]
-      (let [{:keys [guard-pos visited-positions]} (case guard-direction
-                                                    :up (guard-up obstacles g)
-                                                    :down (guard-down  obstacles max-y g)
-                                                    :right (guard-right obstacles max-x  g)
-                                                    :left (guard-left obstacles g)
-                                                    {:guard-pos nil,
-                                                     :visited-positions #{}})]
-        (recur guard-pos (set/union visited-pos visited-positions)))
-      visited-pos)))
+    (let [current-g (last g)]
+      (if (some #(= % current-g) (butlast g))
+        {:guard-pos current-g
+         :loop? true
+         :visited-positions visited-pos}
+        (if-let [guard-direction (:direction current-g)]
+          (let [{:keys [guard-pos visited-positions]} (case guard-direction
+                                                        :up (guard-up obstacles current-g)
+                                                        :down (guard-down  obstacles max-y current-g)
+                                                        :right (guard-right obstacles max-x  current-g)
+                                                        :left (guard-left obstacles current-g)
+                                                        {:guard-pos nil,
+                                                         :visited-positions #{}})]
+            (recur (conj g guard-pos) (set/union visited-pos visited-positions)))
+          {:guard-pos g
+           :loop? false
+           :visited-positions visited-pos})))))
+
 
 (comment
-  (range 1 6)
+
+  ;; Part 1
   (let [puzzle-formated (-> puzzle-input
                             (str/split #"\n"))
         {:keys [obstacle guard]} (->> puzzle-formated
@@ -129,10 +134,87 @@
                                       mark-elements
                                       (apply concat)
                                       (group-by :el-type))
+        guard (first guard)
+        guard-initial [{:x         (:x guard)
+                        :y         (:y guard)
+                        :direction (get guard-patterns (:el guard))}]
+
         max-x (dec (count (first puzzle-formated)))
         max-y (dec (count puzzle-formated))]
-    (visited-positions {:max-x max-x :max-y max-y} obstacle (first guard)))
-  
-  (count "abc")
-  ;;
+    (->> (visited-positions {:max-x max-x :max-y max-y} obstacle guard-initial)
+         :visited-positions
+         count))
+
+
+  (let [puzzle-formated (-> (slurp "src/kaspazza/2024/6/data.txt")
+                            (str/split #"\n"))
+        {:keys [obstacle guard]} (->> puzzle-formated
+                                      (map #(str/split % #""))
+                                      mark-elements
+                                      (apply concat)
+                                      (group-by :el-type))
+        guard (first guard)
+        guard-initial [{:x         (:x guard)
+                        :y         (:y guard)
+                        :direction (get guard-patterns (:el guard))}]
+
+
+        max-x (dec (count (first puzzle-formated)))
+        max-y (dec (count puzzle-formated))]
+    (->> (visited-positions {:max-x max-x :max-y max-y} obstacle guard-initial)
+         :visited-positions
+         count))
+    ;; Part 2
+    ;; Basically we are looking for a moment where after 4 moves, guard position is the same... Hmm no, it could be more than 4... 
+    ;; So basically we are looking for a moment where after X moves, guard position is the same
+    ;; So one thing is about finding them at all.
+    ;; But simple first step would be how to notice we are in a one. So we need to basicaly have loop finding.
+    ;; Simple solution I see rn is to keep all past guard positions and if new one matches it we found a loop.  
+    ;; Then it is to add one new obstacle on all of the visited ones and search through them to count looping ones.
+  (let [puzzle-formated (-> puzzle-input
+                            (str/split #"\n"))
+        {:keys [obstacle guard]} (->> puzzle-formated
+                                      (map #(str/split % #""))
+                                      mark-elements
+                                      (apply concat)
+                                      (group-by :el-type))
+        guard (first guard)
+
+        max-x (dec (count (first puzzle-formated)))
+        max-y (dec (count puzzle-formated))
+        guard-initial [{:x (:x guard) :y (:y guard) :direction (get guard-patterns (:el guard))}]
+        guard-positions (-> (visited-positions {:max-x max-x :max-y max-y} obstacle guard-initial)
+                            :visited-positions
+                            (set/difference #{{:x (:x guard) :y (:y guard)}}))]
+    (->> guard-positions
+         (map
+          #(conj obstacle %))
+         (map
+          #(visited-positions {:max-x max-x :max-y max-y} % guard-initial))
+         (filter :loop?)
+         count))
+
+  (let [puzzle-formated (-> (slurp "src/kaspazza/2024/6/data.txt")
+                            (str/split #"\n"))
+      {:keys [obstacle guard]} (->> puzzle-formated
+                                    (map #(str/split % #""))
+                                    mark-elements
+                                    (apply concat)
+                                    (group-by :el-type))
+      guard (first guard)
+
+      max-x (dec (count (first puzzle-formated)))
+      max-y (dec (count puzzle-formated))
+      guard-initial [{:x (:x guard) :y (:y guard) :direction (get guard-patterns (:el guard))}]
+      guard-positions (-> (visited-positions {:max-x max-x :max-y max-y} obstacle guard-initial)
+                          :visited-positions
+                          (set/difference #{{:x (:x guard) :y (:y guard)}}))]
+  (->> guard-positions
+       (map
+        #(conj obstacle %))
+       (map
+        #(visited-positions {:max-x max-x :max-y max-y} % guard-initial))
+       (filter :loop?)
+       count))
+
 )
